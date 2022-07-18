dd710f85eb9dee3cc921bbfae192eace49186d03
benjamin kisenge <benkisenge03@gmail.com>
2022-07-18 13:43:23 +0200

    add event to js file

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 let faBars = document.querySelector('.fa-bars')
+let menu = document.querySelector('.menu')
+let faXmark = document.querySelector('.fa-xmark')
+let menuItem = document.querySelectorAll('.menu-item')
+
+const displayMenu = () => {
+    menu.classList.add('active-menu')
+}
+const disableMenu = () => {
+    menu.classList.remove('active-menu')
+}
+
+faBars.addEventListener('click',displayMenu)
+faXmark.addEventListener('click',disableMenu)
+menuItem.forEach(item => {
+    item.addEventListener('click',disableMenu) 
+})
