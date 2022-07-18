d6f413d8cc4a1361c9a3ffdd9008d4fdfa76ca82
benjamin kisenge <benkisenge03@gmail.com>
2022-07-18 19:15:27 +0200

    add pop up template

--- script.js
+++ script.js
@@ -15,6 +15,57 @@ const projects = [
         nihil assumenda sapiente doloribus, a sequi 
         alias? Neque odit autem distinctio laboriosam,
          a veritatis placeat enim modi ratione omnis dolores. `,
+    },
+    {
+        title : "Tonic",
+        languages : ['Html','Css','Javascipt','github','ruby','Boostrap'],
+        image: './images/project5.png',
+        descriptio : `    Lorem ipsum dolor sit amet, consectetur adipisicing 
+        elit. Similique quasi atque harum, optio ex 
+        tempore ullam officiis? Obcaecati dolor 
+        placeat, laboriosam assumenda aliquid dicta 
+        esse sequi tempore. Ratione repudiandae non 
+        nostrum ab explicabo labore. Hic neque 
+        quisquam tempore provident labore. Excepturi 
+        sed optio velit voluptas? Recusandae maiores 
+        natus vitae qui tempora minus dolores iste 
+        nihil assumenda sapiente doloribus, a sequi 
+        alias? Neque odit autem distinctio laboriosam,
+         a veritatis placeat enim modi ratione omnis dolores. `,
+    },
+    {
+        title : "Tonic",
+        languages : ['Html','Css','Javascipt','github','ruby','Boostrap'],
+        image: './images/project5.png',
+        descriptio : `    Lorem ipsum dolor sit amet, consectetur adipisicing 
+        elit. Similique quasi atque harum, optio ex 
+        tempore ullam officiis? Obcaecati dolor 
+        placeat, laboriosam assumenda aliquid dicta 
+        esse sequi tempore. Ratione repudiandae non 
+        nostrum ab explicabo labore. Hic neque 
+        quisquam tempore provident labore. Excepturi 
+        sed optio velit voluptas? Recusandae maiores 
+        natus vitae qui tempora minus dolores iste 
+        nihil assumenda sapiente doloribus, a sequi 
+        alias? Neque odit autem distinctio laboriosam,
+         a veritatis placeat enim modi ratione omnis dolores. `,
+    },
+    {
+        title : "Multi - Post Stories",
+        languages : ['Html','Css','Javascipt','github','ruby','Boostrap'],
+        image: './images/project5.png',
+        descriptio : `    Lorem ipsum dolor sit amet, consectetur adipisicing 
+        elit. Similique quasi atque harum, optio ex 
+        tempore ullam officiis? Obcaecati dolor 
+        placeat, laboriosam assumenda aliquid dicta 
+        esse sequi tempore. Ratione repudiandae non 
+        nostrum ab explicabo labore. Hic neque 
+        quisquam tempore provident labore. Excepturi 
+        sed optio velit voluptas? Recusandae maiores 
+        natus vitae qui tempora minus dolores iste 
+        nihil assumenda sapiente doloribus, a sequi 
+        alias? Neque odit autem distinctio laboriosam,
+         a veritatis placeat enim modi ratione omnis dolores. `,
     }
 ]
 const faBars = document.querySelector('.fa-bars');
